cb4f50ac2d5ef2a9604feadb522b64e7ea4b9ef7
bgaze <benjamin@bgaze.fr>
2018-05-31 09:54:06 +0400

    Create class content Update phpdoc

--- src/Console/PhpCsFixerDescribe.php
+++ src/Console/PhpCsFixerDescribe.php
@@ -3,11 +3,11 @@
 namespace Bgaze\LaravelPhpCsFixer\Console;
 
 use Illuminate\Console\Command;
-use Symfony\Component\Console\Input\InputOption;
-use Symfony\Component\Console\Input\InputArgument;
 
 /**
- * The PhpCsFixerDescribe class.
+ * This Console application describes a rule or ruleset of PHP-CS-Fixer.
+ * 
+ * @author bgaze <benjamin@bgaze.fr>
  */
 class PhpCsFixerDescribe extends Command {
 
@@ -31,46 +31,7 @@ class PhpCsFixerDescribe extends Command {
      * @return mixed
      */
     public function handle() {
-        $params = [];
-
-        // Prepare arguments.
-
-        foreach ($this->argument('path') as $path) {
-            $params[] = '"' . base_path($path) . '"';
-        }
-
-        // Prepare options.
-
-        $params[] = '--path-mode="' . $this->option('path-mode') . '"';
-
-        $params[] = '--allow-risky="' . ($this->option('allow-risky') ? 'yes' : 'no') . '"';
-
-        $params[] = '--using-cache="' . ($this->option('using-cache') ? 'yes' : 'no') . '"';
-
-        if ($this->option('config') && file_exists(base_path($this->option('config')))) {
-            $params[] = '--config="' . base_path($this->option('config')) . '"';
-        } elseif (file_exists(base_path('.php_cs'))) {
-            $params[] = '--config="' . base_path('.php_cs') . '"';
-        } else {
-            $params[] = '--config="' . __DIR__ . '/../../config/.php_cs"';
-        }
-
-        foreach (['dry-run', 'stop-on-violation', 'diff', 'quiet', 'verbose', 'ansi', 'no-ansi', 'no-interaction'] as $option) {
-            if ($this->option($option)) {
-                $params[] = "--$option";
-            }
-        }
-
-        foreach (['rules', 'cache-file', 'format', 'show-progress'] as $option) {
-            if ($this->option($option)) {
-                $params[] = "--$option=\"" . $this->option($option) . '"';
-            }
-        }
-
-        // Fix files.
-
-        chdir(base_path());
-        passthru(base_path('vendor/bin/php-cs-fixer') . ' fix ' . implode(' ', $params));
+        passthru(base_path('vendor/bin/php-cs-fixer') . ' describe ' . $this->argument('name'));
     }
 
 }
