1652ba22e0111d4873927dcb576b0b15a0ec8a75
Benjamin Gazé <benjamin@bgaze.fr>
2020-02-23 13:08:41 +0400

    Refactor helper

--- src/helpers.php
+++ src/helpers.php
@@ -1,22 +1,21 @@
 <?php
 
+use Illuminate\Support\Facades\Artisan;
+
 /**
  * Fix file(s) / dir(s) using PhpCsFixer.
- * 
- * @param mixed $path 
- * A path or an array of pathes
- * 
- * @param array $options
+ *
+ * @param  mixed  $path
+ * A path or an array of paths
+ *
+ * @param  array  $options
  * An array of options
- * 
+ *
  * @return boolean
  */
-function php_cs_fixer($path, array $options = []) {
-    if (!is_array($path)) {
-        $path = [$path];
-    }
-
-    $options = array_merge($options, ['path' => $path]);
-    
-    return Artisan::call('php-cs-fixer:fix', $options);
+function php_cs_fixer($path, array $options = [])
+{
+    return Artisan::call('php-cs-fixer:fix', array_merge($options, [
+        'path' => (array) $path
+    ]));
 }
